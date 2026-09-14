0347abc6c287bd6a9d69df43d64b9cb43461aa55
sksiddharth641-max <sksiddharth641@gmail.com>
2026-09-15 02:19:34 +0530

    Add review routes for creating reviews

---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { createReview } = require("../controllers/reviewController");
+
+router.post("/", createReview);
+
+module.exports = router;
