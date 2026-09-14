0d0f7bbaba60bf9644c1dea2c385924b515d377f
sksiddharth641-max <sksiddharth641@gmail.com>
2026-09-15 02:22:53 +0530

    Add review routes to the main router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const contactRoutes = require("./contactRoutes");
+const reviewRoutes = require("./reviewRoutes");
 
 router.use("/contact", contactRoutes);
+router.use("/reviews", reviewRoutes);
 
 router.get("/", (req, res) => {
 
