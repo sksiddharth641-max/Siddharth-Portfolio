1f53e13c93999e44312c1e1016dcdc98b5640cb2
sksiddharth641-max <sksiddharth641@gmail.com>
2026-09-15 11:49:47 +0530

    Add getApprovedReviews route to reviewRoutes

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const {
   createReview,
   getPendingReviews,
+  getApprovedReviews,
   approveReview,
   rejectReview
+  
 } = require("../controllers/reviewController");
+} = require("../controllers/reviewController");
+
+router.get("/pending", getPendingReviews);
 
 router.get("/pending", getPendingReviews);
 
